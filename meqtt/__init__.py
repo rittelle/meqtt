6cabea3c14d557f83b607639cb2380058b2c135e
rittelle <33598633+rittelle@users.noreply.github.com>
2023-05-17 00:07:03 +0200

    Export the public API in the main module
    For easier access and a better overview of what the public API actually
    is.

--- meqtt/__init__.py
+++ meqtt/__init__.py
@@ -1,3 +1,6 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 from .messages import Message, message
+from .connection import Connection
+from .runners import run_process, launch_process
+from .process import Process, task
