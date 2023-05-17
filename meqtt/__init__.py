be6b852f8f8fb328f5340fd316b5feeb072b5a93
rittelle <33598633+rittelle@users.noreply.github.com>
2023-05-17 23:03:57 +0200

    Implement message handlers
    Implement message handlers on the Process class.

--- meqtt/__init__.py
+++ meqtt/__init__.py
@@ -3,4 +3,4 @@ __version__ = "0.1.0"
 from .messages import Message, message
 from .connection import Connection
 from .runners import run_process, launch_process
-from .process import Process, task
+from .process import Process, task, handler
--- meqtt/process.py
+++ meqtt/process.py
@@ -1,37 +1,61 @@
 import asyncio
-from collections import defaultdict
-from typing import Optional
-
+import inspect
 import itertools
+from collections import defaultdict
+from typing import List, Optional, Type
 
+import meqtt.connection as connection  # module import to avoid circular import
 from meqtt.messages import Message
-from meqtt.connection import Connection
 
 
 class Process:
     def __init__(self):
-        # map von methode zu List[asyncio.Task]
-        self.__running_tasks = defaultdict(list)
-        # map von Klasse zu Handlern
-        self.__handlers = {}
-        # liste von gerade laufenden handlern
-        self.__running_handlers = []
+        # Dict from task methods bound to self to running tasks of
+        # that method.
+        self.__running_tasks = {}
+        # Dict from message types to lists of handlers that take
+        # objects of those messages.  The handlers are bound to self.
+        self.__handlers = defaultdict(list)
+        # A list of tasks returned by handlers that are currently
+        # running.
+        self.__running_handlers: List[asyncio.Task] = []
+        # The name of this process.
+        # TODO: Make sure that this is unique.
         self.name = str(type(self).__name__)
+
+        # The connection to the broker.
+        self._connection: Optional["connection.Connection"] = None
 
         self._scan_methods()
 
-    async def start(self, connection: Connection):
+    @property
+    def is_running(self) -> bool:
+        """Gibt an, ob der Prozess läuft."""
+
+        return self._connection is not None
+
+    async def start(self, connection: "connection.Connection"):
         """Startet den Prozess."""
 
+        if self.is_running:
+            raise RuntimeError("Process is already running")
         self._connection = connection
         await self.on_start()
 
     async def stop(self):
         """Beendet den Prozess, verbindung zum Broker noch vorhanden."""
 
+        if not self.is_running:
+            raise RuntimeError("Process is not running")
         await self.on_stop()
         self._kill_remaining_tasks()
         self._connection = None
+
+    @property
+    def message_classes(self) -> List[Type[Message]]:
+        """Gibt alle Message-Klassen zurück, die dieser Prozess verarbeiten kann."""
+
+        return list(self.__handlers.keys())
 
     async def kill(self):
         """Beendet den Prozess, die Verbindung zum Broker ist nicht mehr vorhanden."""
@@ -39,6 +63,14 @@ class Process:
         await self.on_kill()
         self._kill_remaining_tasks()
         self._connection = None
+
+    async def handle_message(self, message: Message):
+        """Verarbeitet eine Nachricht."""
+
+        message_type = type(message)
+        handlers = self.__handlers.get(message_type, [])
+        for handler in handlers:
+            await handler(message)
 
     async def join(self):
         """Wartet auf das Beenden des Prozesses."""
@@ -124,17 +156,58 @@ class Process:
     #     gerade etwas anderes tut.
     #     """
 
+    @staticmethod
+    def _get_message_type_for_handler(method):
+        method_name = method.__name__
+
+        # get the signature
+        try:
+            signature = inspect.signature(method)
+        except TypeError as exc:
+            raise ValueError(f"Cannot get signature of method {method_name}") from exc
+
+        # get the message parameter and check the number of parameters
+        parameters = list(signature.parameters.values())
+        if len(parameters) != 1:  # methods are bound at this point
+            raise ValueError(
+                f"A handler has to take a message as the only parameter in addition to self: {method_name}"
+            )
+        message_parameter = parameters[0]
+
+        # check if the parameter is positional
+        if message_parameter.kind not in (
+            inspect.Parameter.POSITIONAL_OR_KEYWORD,
+            inspect.Parameter.POSITIONAL_ONLY,
+        ):
+            raise ValueError(
+                f"The message parameter of a handler has to allow being passed positionally: {method_name}"
+            )
+
+        # get and check the type annotation
+        message_type = message_parameter.annotation
+        has_type_annotation = message_type is not inspect.Parameter.empty
+        is_message_type = issubclass(message_type, Message)
+        if not has_type_annotation or not is_message_type:
+            raise ValueError(
+                f"The message parameter has to have a Message class as the type annotation: {method_name}"
+            )
+
+        return message_type
+
     def _scan_methods(self):
         """Scan the methods of the class for handlers and tasks."""
 
         for method_name in dir(self):
             method = getattr(self, method_name)
             if hasattr(method, "_meqtt_type"):
-                # if method._meqtt_type == "handler":
-                # TODO: get the type of the first argument of method
-                # self.__handlers[message_cls] = method
-                if method._meqtt_type == "task":
-                    self.__running_tasks[method] = []
+                match method._meqtt_type:
+                    case "handler":
+                        message_type = self._get_message_type_for_handler(method)
+                        # bind the method to self
+                        bound_method = method.__get__(self, self.__class__)
+                        self.__handlers[message_type].append(bound_method)
+                    case "task":
+                        self.__running_tasks[method] = []
 
     def _kill_remaining_tasks(self):
         """Beendet alle verbleibenden Tasks."""
@@ -153,4 +226,13 @@ def task(method):
     if not inspect.iscoroutinefunction(method):
         raise ValueError(f"Tasks have to be async: {method.__name__}")
     method._meqtt_type = "task"
+    return method
+
+
+def handler(method):
+    """Dekoator, der eine Methode als Handler markiert."""
+
+    if not inspect.iscoroutinefunction(method):
+        raise ValueError(f"Tasks have to be async: {method.__name__}")
+    method._meqtt_type = "handler"
     return method
