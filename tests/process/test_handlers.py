194d940fa22006c9250f4fe519a54b5e387c2987
rittelle <33598633+rittelle@users.noreply.github.com>
2023-05-17 23:04:51 +0200

    Add tests for the message handlers
    Test the new message handler support on the Process class – at least a
    bit.

---
+++ tests/process/test_handlers.py
@@ -0,0 +1,98 @@
+import logging
+import meqtt
+import pytest
+
+_log = logging.getLogger(__name__)
+
+
+@pytest.mark.asyncio
+async def test_different_messages_in_handlers():
+    @meqtt.message("test")
+    class TestMessage(meqtt.Message):
+        idx = 1 # allows to check for the correct message
+
+    @meqtt.message("test2")
+    class TestMessage2(meqtt.Message):
+        idx = 2
+
+    class AProcess(meqtt.Process):
+        def __init__(self):
+            super().__init__()
+            self.calls_to_test1 = 0
+            self.calls_to_test2 = 0
+
+
+        @meqtt.handler
+        async def on_test(self, message: TestMessage):
+            assert message.idx == 1
+            self.calls_to_test1 += 1
+
+        @meqtt.handler
+        async def on_test2(self, message: TestMessage2):
+            assert message.idx == 2
+            self.calls_to_test2 += 1
+
+    _log.debug("Process class created")
+    process = AProcess()
+    _log.debug("Process instance created")
+    await process.handle_message(TestMessage())
+    assert process.calls_to_test1 == 1
+    assert process.calls_to_test2 == 0
+    await process.handle_message(TestMessage2())
+    assert process.calls_to_test1 == 1
+    assert process.calls_to_test2 == 1
+    _log.debug("Process handlers executed successfully")
+
+@pytest.mark.asyncio
+async def test_same_message_in_handlers():
+    @meqtt.message("test")
+    class TestMessage(meqtt.Message):
+        pass
+
+    class AProcess(meqtt.Process):
+        def __init__(self):
+            super().__init__()
+            self.calls_to_test1 = 0
+            self.calls_to_test2 = 0
+
+        @meqtt.handler
+        async def on_test1(self, message: TestMessage):
+            self.calls_to_test1 += 1
+
+        @meqtt.handler
+        async def on_test2(self, message: TestMessage):
+            self.calls_to_test2 += 1
+
+    _log.debug("Process class created")
+    process = AProcess()
+    _log.debug("Process instance created")
+    await process.handle_message(TestMessage())
+    assert process.calls_to_test1 == 1
+    assert process.calls_to_test2 == 1
+    _log.debug("Process handlers executed successfully")
+
+
+@pytest.mark.asyncio
+async def test_if_unrelated_handler_does_not_get_executed():
+    @meqtt.message("test")
+    class TestMessage(meqtt.Message):
+        pass
+
+    @meqtt.message("test2")
+    class TestMessage2(meqtt.Message):
+        pass
+
+    class AProcess(meqtt.Process):
+        @meqtt.handler
+        async def on_test1(self, message: TestMessage):
+            pass
+
+        @meqtt.handler
+        async def on_test2(self, message: TestMessage2):
+            assert False, "This handler should not be executed"
+
+    _log.debug("Process class created")
+    process = AProcess()
+    _log.debug("Process instance created")
+    await process.handle_message(TestMessage())
+    _log.debug("Process handlers executed successfully")
